0651b5f69375cf43f40838a6bfaa6371468711e7
damiancaelen <475794@student.fontys.nl>
2025-01-24 21:51:26 +0100

    added first files

---
+++ SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Damian Caelen on 24/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
